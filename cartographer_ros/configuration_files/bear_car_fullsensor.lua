dbb55dd0b4bfb651856d656b6e456bf80fe8da14
sivanak1 <krishnananda.sivananda@aalto.fi>
2019-12-02 23:11:49 +0200

    full sensor config

--- cartographer_ros/configuration_files/bear_car_fullsensor.lua
+++ cartographer_ros/configuration_files/bear_car_fullsensor.lua
@@ -24,13 +24,13 @@ options = {
   odom_frame = "odom",
   provide_odom_frame = true,
   publish_frame_projected_to_2d = false,
-  use_odometry = false,
+  use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
   num_laser_scans =1,
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
-  num_point_clouds = 0,
+  num_point_clouds = 1,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
@@ -44,19 +44,17 @@ options = {
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 
-TRAJECTORY_BUILDER_2D.min_range = 0.1
-TRAJECTORY_BUILDER_2D.max_range = 4.0
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 2.0
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 35
+TRAJECTORY_BUILDER_2D.min_range = 0.3
+TRAJECTORY_BUILDER_2D.max_range = 8.
+TRAJECTORY_BUILDER_2D.missing_data_ray_length = 1.
 TRAJECTORY_BUILDER_2D.use_imu_data = false
-TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 0.3
-TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.2
-TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(3.)
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 70
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 300
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 10.
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 1e-1
 
-TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.resolution = 0.035
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 120
-POSE_GRAPH.optimize_every_n_nodes = 100
-POSE_GRAPH.constraint_builder.min_score = 0.82
-POSE_GRAPH.constraint_builder.sampling_ratio = 1.
+POSE_GRAPH.optimization_problem.huber_scale = 1e2
+POSE_GRAPH.optimize_every_n_nodes = 35
+POSE_GRAPH.constraint_builder.min_score = 0.65
 return options
